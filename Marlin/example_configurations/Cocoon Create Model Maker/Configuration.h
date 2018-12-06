fff919516cf7955aad406bfcefb3e25195c879dd
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2018-12-06 11:50:38 -0800

    Board Name Cleanup
    Updated Wanhao board name/reference to be all caps to stay consistent with other boardsn in Marlin.

--- Marlin/example_configurations/Cocoon Create Model Maker/Configuration.h
+++ Marlin/example_configurations/Cocoon Create Model Maker/Configuration.h
@@ -131,7 +131,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_ONEPLUS1_wanhao
+  #define MOTHERBOARD BOARD_ONEPLUS1_WANHAO
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
